0c767381669e56e0b39d2f889c889f54c92fe7e4
Kryštof Matějka <krystof.matejka@pipedrive.com>
2022-01-12 21:21:04 +0100

    install pigeon maps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createGlobalStyle} from 'styled-components'
+import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -12,6 +13,10 @@ const GlobalStyle = createGlobalStyle`
 const SpringApp = ({Component, pageProps}) => {
   return (
     <>
+      <Head>
+        <title>History in Motion</title>
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css' rel='stylesheet' />
+      </Head>
       <GlobalStyle/>
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import {Header} from '@/src/components/header'
+import { Map, Marker } from 'pigeon-maps'
+import { stamenTerrain } from 'pigeon-maps/providers'
+
+const WorldMap = () => {
+  return (
+    <Map
+      height={'100vh' as any}
+      defaultCenter={[37.98381, 23.72754]}
+      defaultZoom={8}
+      provider={stamenTerrain}
+    >
+      <Marker
+        width={50}
+        anchor={[37.98381, 23.72754]}
+        hover={true}
+        payload={'Athens'}
+        onClick={(p) => console.log('onClick', p)}
+      />
+    </Map>
+  )
+}
 
 const Home = () => {
   return (
-    <Header text='Heading'/>
+    <WorldMap/>
   )
 }
 
