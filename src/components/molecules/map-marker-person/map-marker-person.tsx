1733123d955ab7ef93841a2c95f5158ef2f55050
Kryštof Matějka <krystof.matejka@pipedrive.com>
2022-01-15 13:11:16 +0100

    fix part of problem with animation

--- src/components/molecules/map-marker-person/map-marker-person.tsx
+++ src/components/molecules/map-marker-person/map-marker-person.tsx
@@ -1,10 +1,18 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect, useRef} from 'react'
 import styled from 'styled-components'
 import {Overlay} from 'pigeon-maps'
 import {MarkerPerson} from '@/src/components/atoms/marker-person'
 
-const StyledOverlay = styled(Overlay)`
-  transition: 1s ease-in-out;
+const anchorDiff = (a: number[], b: number[]) => !(a[0] === b[0] && a[1] === b[1])
+
+type StyledOverlayProps = {
+  animate: boolean
+}
+
+const StyledOverlay = styled(Overlay).attrs<StyledOverlayProps>((props) => ({
+  animate: props.animate,
+}))<StyledOverlayProps>`
+  transition: ${p => p.animate ? 'transform 1s linear' : 'none'};
 `
 
 type MapMarkerPersonProps = {
@@ -13,8 +21,14 @@ type MapMarkerPersonProps = {
 }
 
 export const MapMarkerPerson: FC<MapMarkerPersonProps> = ({name, ...props}) => {
+  const anchorRef = useRef([0, 0])
+
+  useEffect(() => {
+    anchorRef.current = props.anchor
+  }, [props])
+
   return (
-    <StyledOverlay {...props}>
+    <StyledOverlay {...props} animate={anchorDiff(props.anchor, anchorRef.current)}>
       <MarkerPerson name={name}/>
     </StyledOverlay>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const Slider = ({ value, onValueChange }) => {
         type={'range'}
         min={-600}
         max={-400}
-        step={1}
+        step={10}
         value={value}
         onChange={e => onValueChange(parseInt(e.target.value))}
       />
